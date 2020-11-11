ce65b68790a1a762a1345d057df8247a50778ef0
Aneudy Adames <a4aneudy@gmail.com>
2020-11-10 22:59:00 -0500

    preparing for deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_KEY: '313d79d4-481f-4b07-950e-33c5275532a6',
+  API_KEY: process.env.REACT_APP_API_KEY,
   API_ENDPOINT: 'https://nameless-crag-11831.herokuapp.com/api/',
 };
 
