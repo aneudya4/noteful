b5841e3aa632412963d12e5d7c251e93ae025e0e
Aneudy Adames <a4aneudy@gmail.com>
2021-05-27 15:26:28 -0400

    updated endpoint for server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: 'https://nameless-crag-11831.herokuapp.com/api',
+	API_KEY: process.env.REACT_APP_API_KEY,
+	API_ENDPOINT: 'https://serene-stream-03487.herokuapp.com/api',
 };
 
 export default config;
